7c0216ebcaa0638df18f144e67d2afb2d9681b17
Connor Couetil <couetilc@gmail.com>
2025-09-29 13:13:19 -0400

    include the logo as an example of different files Vite handles for us

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,9 +1,11 @@
 import './style.css'
 import { createRoot } from 'react-dom/client'
+import SousChefLogo from './souschef-logo.png';
 
 export default function App(props) {
   return (
     <div>
+      <img height="200px" src={SousChefLogo} />
       <h1>Hello, World!</h1>
       <p>It's us, <strong>Team 21</strong>, living the dream</p>
     </div>
