96501e4b078b8dac744c77f75088d123d52839c0
Connor Couetil <couetilc@gmail.com>
2025-09-29 15:09:21 -0400

    added front-end called that calls our new api endpoint

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,13 +1,27 @@
 import './style.css'
 import { createRoot } from 'react-dom/client'
-import SousChefLogo from './souschef-logo.png';
+import SousChefLogo from './souschef-logo.png'
+import { useEffect, useState } from 'react'
 
 export default function App(props) {
+
+  const [health, setHealth] = useState({})
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/health')
+      .then(res => res.json())
+      .then(json => setHealth(json))
+  }, [])
+
   return (
     <div>
       <img height="200px" src={SousChefLogo} />
       <h1>Hello, World!</h1>
       <p>It's us, <strong>Team 21</strong>, living the dream</p>
+
+      <pre>
+        {JSON.stringify(health, null, 2)}
+      </pre>
     </div>
   )
 }
