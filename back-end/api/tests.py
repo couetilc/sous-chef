de3b545e9c87cd6f9423a9fab2b0277e69ca9740
Connor Couetil <couetilc@gmail.com>
2025-09-29 14:48:55 -0400

    implemented an API endpoint, health_check/, as an example to learn how to work in these repo. Here are instructions for running this:
    1. Run the setup instructions from the README.
    2. Run `python manage.py test`
    3. Run `python manage.py runserver`
    4. Visit `http://localhost:8000/health

--- back-end/api/tests.py
+++ back-end/api/tests.py
@@ -6,3 +6,10 @@ class HelloWorldTestCases(TestCase):
     def test_successful_response(self):
         response = self.client.get(reverse('index'))
         self.assertContains(response, "Hello, World!")
+
+class HealthCheckTestCases(TestCase):
+    def test_successful_response(self):
+        response = self.client.get(reverse('health_check'))
+        self.assertContains(response, "status")
+        self.assertContains(response, "current_time")
+        self.assertContains(response, "uptime")
--- back-end/api/views.py
+++ back-end/api/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from datetime import datetime, timezone
+
+bootup_time = datetime.now(timezone.utc)
 
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello, World!</h1>")
+
+def health_check(request):
+    now = datetime.now(timezone.utc)
+    current_time = now.isoformat()
+    uptime = str(now - bootup_time)
+    return JsonResponse({
+        'status': 'healthy',
+        'current_time': current_time,
+        'uptime': uptime,
+    })
