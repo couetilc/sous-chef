3a197bf0181653b7a4f67ef466f6408ba2e60d16
Connor Couetil <couetilc@gmail.com>
2025-09-29 14:32:27 -0400

    single test for the api

--- back-end/api/tests.py
+++ back-end/api/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class HelloWorldTestCases(TestCase):
+    def test_successful_response(self):
+        response = self.client.get(reverse('index'))
+        self.assertContains(response, "Hello, World!")
