48ebad2344288918ef81387850ac2aac7c3b7cc0
Connor Couetil <couetilc@gmail.com>
2025-09-30 16:24:24 -0400

    oauth2 library is integrated. I followed these instructions https://django-oauth-toolkit.readthedocs.io/en/latest/rest-framework/getting_started.html

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
+from oauth2_provider import urls as oauth2_urls
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('admin/', admin.site.urls),
+    path('o/', include(oauth2_urls)),
+    path('users/', views.UserList.as_view()),
+    path('users/<pk>/', views.UserDetails.as_view()),
+    path('groups/', views.GroupList.as_view()),
 ]
--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oauth2_provider',
     'rest_framework',
     'corsheaders',
     'api',
@@ -133,4 +134,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
 'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
+'DEFAULT_AUTHENTICATION_CLASSES': ('oauth2_provider.contrib.rest_framework.OAuth2Authentication'),
+'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated'),
 }
+
+OAUTH2_PROVIDER = {
+    # this is the list of available scopes
+    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
+}
+
+LOGIN_URL = '/admin/login'
