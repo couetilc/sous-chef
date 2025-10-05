3b04e6a92215fd4a1bb281b68cf9db13897c01f1
Asehr1 <asehr@purdue.edu>
2025-10-05 14:04:43 -0400

    Added navigation to home page and logged-in user tracking

--- front-end/src/login.jsx
+++ front-end/src/login.jsx
@@ -1,47 +1,85 @@
-// /src/Login
+// /src/login
+import home from './home.jsx'
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import './style.css';
+import SousChefLogo from './souschef-logo.png';
 
 // constants for username and password testing
-const user = "user";
-const pw = "pw";
+const userCheck = "user";
+const pwCheck = "pw";
 
-function checkLogin() {
-  const userElement = document.getElementById("userId");
-  if ( userElement.value == '') {
-    alert("Please enter your username!");
-    return;
-  }
-  const userText = userElement.value;
 
-  const pwElement = document.getElementById("pwId");
-  if ( pwElement.value =='' ) {
-    alert("Please enter your password!");
-    return;
-  }
-  const pwText = pwElement.value;
 
-  if ( !user.localeCompare(userText) && !pw.localeCompare(pwText) ) {
-    alert("login successful!");
-  }
-  else {
-    alert("login failed, incorrect username-password pair!");
-    console.log(userText);
-    console.log(pwText);
-  }
+function navToCreate() {
+  //navigate to Create Account page
+  const navigate = useNavigate;
+  return navigate("/create-account");
 }
 
 
-export default function Login() {
+export default function Login(props) {
+  let user = props.user;
+  let setUser = props.setUser;
+  const navigate = useNavigate();
+
+  function checkLogin() {
+    function navToHome() {
+      return navigate("/home");
+    }
+    const userElement = document.getElementById("userId");
+    if ( userElement.value == '') {
+      alert("Please enter your username!");
+      return;
+    }
+    const userText = userElement.value;
+
+    const pwElement = document.getElementById("pwId");
+    if ( pwElement.value =='' ) {
+      alert("Please enter your password!");
+      return;
+    }
+    const pwText = pwElement.value;
+
+    if ( !userCheck.localeCompare(userText) && !pwCheck.localeCompare(pwText) ) {
+      alert("login successful!");
+      setUser(userText);
+      navToHome();
+    }
+    else {
+      alert("login failed, incorrect username-password pair!");
+      console.log(userText);
+      console.log(pwText);
+    }
+  }
+  const loginDiv = {
+    border: '5px solid black',
+    backgroundColor: 'goldenrod',
+    textAlign: 'center'
+  };
   return (
-    <div>
-      <p>LOGIN PAGE</p>
+    <div style={loginDiv}>
+        <h1>Log In</h1>
         <label>Username: <input name="userIn" id="userId" /> </label>
         <br />
         <br />
         <label>Password: <input type="password" name="passIn" id="pwId"/> </label>
         <br />
-        <button className="login-button" type="button" onClick={checkLogin}>
-          Log In
+        <div className="inline-div" >
+        <button className="button"
+                type="button"
+                style={{backgroundColor: 'tomato', color: 'white'}}
+                onClick={navToCreate}>
+          Create Account
         </button>
+        <div style={{width: '10px'}}></div>
+        <button className="button"
+                type="button"
+                style={{backgroundColor: 'green', color: 'white'}}
+                onClick={checkLogin}>
+          Continue
+        </button>
+        </div>
     </div>
   )
 }
