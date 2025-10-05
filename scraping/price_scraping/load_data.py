59cefb610b9ce2da551bbab6b964c79313287ffa
Branton Ling <brantonling05@gmail.com>
2025-10-05 19:26:00 +0000

    finalized prices loading script

--- scraping/price_scraping/load_data.py
+++ scraping/price_scraping/load_data.py
@@ -11,18 +11,18 @@ import psycopg
 import csv
 from pathlib import Path
 
-path = Path("./scraping/ingredient_prices.csv")
+path = Path("./price_scraping/ingredient_prices.csv")
 
 conn_info = os.getenv("DATABASE_URL", "postgresql://dbuser:dbpass@localhost:5432/api")
 
 with psycopg.connect(conn_info, row_factory = psycopg.rows.dict_row) as conn:
     with conn.cursor() as cur:
-        cur.execute("DROP TABLE IF EXISTS ingredient_prices")
-        cur.execute("CREATE TABLE ingredient_prices (id serial PRIMARY KEY, ingredient_name TEXT, quantity TEXT, price TEXT)")
+        cur.execute("DROP TABLE IF EXISTS scraped_prices")
+        cur.execute("CREATE TABLE scraped_prices (id serial PRIMARY KEY, ingredient_name TEXT, quantity TEXT, price TEXT)")
         with path.open(newline="", encoding="utf-8") as f:
             reader = csv.DictReader(f)
             for row in reader:
                 cur.execute(
-                    "INSERT INTO ingredient_prices (ingredient_name, quantity, price) VALUES (%s,%s,%s)",
+                    "INSERT INTO scraped_prices (ingredient_name, quantity, price) VALUES (%s,%s,%s)",
                     (row["ingredient_name"], row["quantity"], row["price"])
                 )
