73c5d4fae3b713a3ca9552fce13a642be52b970b
Dalbert Sun <sun1170@purdue.edu>
2025-10-09 14:04:26 +0000

    Updated load data

--- scraping/ingredient_scraping/load_data.py
+++ scraping/ingredient_scraping/load_data.py
@@ -17,10 +17,10 @@ conn_info = os.getenv("DATABASE_URL", "postgresql://dbuser:dbpass@localhost:5432
 
 with psycopg.connect(conn_info, row_factory = psycopg.rows.dict_row) as conn:
     with conn.cursor() as cur:
-        cur.execute("DROP TABLE IF EXISTS scraped_recipes")
-        cur.execute("CREATE TABLE scraped_recipes (id serial PRIMARY KEY, title TEXT, url TEXT, image TEXT, ingredients TEXT, steps TEXT)")
+        cur.execute("DROP TABLE IF EXISTS scraped_ingredients")
+        cur.execute("CREATE TABLE scraped_ingredients (id serial PRIMARY KEY, name TEXT, calories TEXT, protein TEXT, fat TEXT, carbs TEXT, category TEXT)")
         with path.open(newline="", encoding="utf-8") as f:
             reader = csv.DictReader(f)
             for row in reader:
-                cur.execute("INSERT INTO scraped_recipes (title, url, image, ingredients, steps) VALUES (%s,%s,%s,%s,%s)", (row["title"], row["url"], row["image"], row["ingredients"], row["steps"]))
+                cur.execute("INSERT INTO scraped_ingredients (name, calories, protein, fat, carbs, category) VALUES (%s,%s,%s,%s,%s,%s)", (row["description"], row["calories"], row["protein_g"], row["fat_g"], row["carbs_g"], row["food_category"]))
 
