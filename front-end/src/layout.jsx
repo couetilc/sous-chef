529175909569df6321823ccdbb8c8b1b8781628c
Connor Couetil <couetilc@gmail.com>
2025-10-30 11:01:12 -0400

    simple desktop/mobile layout split, added resize event handler as trigger

--- front-end/src/layout.jsx
+++ front-end/src/layout.jsx
@@ -1,10 +1,18 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import SousChefLogo from './souschef-logo.png';
 import { useLocation } from 'react-router';
 import CurvedEdge from './curvedEdge';
 
+let checkMobile = () => window.innerWidth < 600
+
 export default function Layout(props) {
-  const curLocation = useLocation();
+  const [isMobile, setIsMobile] = useState(checkMobile);
+
+  useEffect(() => {
+    let handler = () => setIsMobile(checkMobile);
+    window.addEventListener('resize', handler)
+    return () => window.removeEventListener('resize', handler)
+  }, []);
 
   const navList = useRef(null);
   useEffect(() => {
@@ -23,39 +31,49 @@ export default function Layout(props) {
     }
   }, [navList])
 
+  console.log(window.innerWidth);
+
+  let CurrentLayout = isMobile ? MobileLayout : DesktopLayout;
+
+  return (
+    <CurrentLayout navRef={navList}>
+      {props.children}
+    </CurrentLayout>
+  )
+}
+
+function Nav(props) {
+  const curLocation = useLocation();
+  return (curLocation.pathname.localeCompare("/") == 0 ||
+    curLocation.pathname.localeCompare("/login/") == 0 ||
+    curLocation.pathname.localeCompare("/create-account/") == 0)
+    ? <img className="sous-chef-logo" src={SousChefLogo} height="300px"/>
+    : <header className="navigation-menu">
+      <a className="nav-link-logo" href="/home">
+        <img className="sous-chef-logo" src={SousChefLogo} width="150px"/>
+      </a>
+      <nav>
+        <ul ref={props.navRef}>
+          <li><a href="/home/">Home</a></li>
+          <li><a href="/sous-chef/">Sous Chef</a></li>
+          <li><a href="/nutritionist/">Nutritionist</a></li>
+          <li><a href="/recipes/">Recipes</a></li>
+          <li><a href="/inventory/">Inventory</a></li>
+          <li><a href="/history/">History</a></li>
+          <li><a href="/settings/">Account Settings</a></li>
+          <li><a href="/logout/">Logout</a></li>
+        </ul>
+      </nav>
+    </header>
+}
+
+function DesktopLayout(props) {
+
   return (
     <div className="app-container">
       <div className="left-menu">
         {/* left menu content */}
-        {( curLocation.pathname.localeCompare("/") == 0 ||
-          curLocation.pathname.localeCompare("/login/") == 0 ||
-          curLocation.pathname.localeCompare("/create-account/") == 0)
-          ? <img className="sous-chef-logo" src={SousChefLogo} height="300px"/>
-          : <header className="navigation-menu">
-            <div className="hamburger-button" onClick={
-              e => getComputedStyle(e.currentTarget)['margin-left'] == '0px'
-                ? e.currentTarget.style['margin-left'] = 'calc(100% - 2em)'
-                : e.currentTarget.style['margin-left'] = '0'
-            }>
-              <span>☰</span>
-            </div>
-            <a className="nav-link-logo" href="/home">
-              <img className="sous-chef-logo" src={SousChefLogo} width="150px"/>
-            </a>
-            <nav>
-              <ul ref={navList}>
-                <li><a href="/home/">Home</a></li>
-                <li><a href="/sous-chef/">Sous Chef</a></li>
-                <li><a href="/nutritionist/">Nutritionist</a></li>
-                <li><a href="/recipes/">Recipes</a></li>
-                <li><a href="/inventory/">Inventory</a></li>
-                <li><a href="/history/">History</a></li>
-                <li><a href="/settings/">Account Settings</a></li>
-                <li><a href="/logout/">Logout</a></li>
-              </ul>
-            </nav>
-          </header>
-        }
+        <Nav />
       </div>
       <div className="center-bar-left" />
       <div className="center-page">
@@ -70,6 +88,31 @@ export default function Layout(props) {
         <div className="center-bottom-bar" />
       </div>
       <div className="center-bar-right" />
+    </div>
+  )
+}
+
+function MobileLayout(props) {
+  return (
+    <div className="app-container">
+      <div className="top-menu">
+        <Nav />
+      </div>
+      <div className="mobile-center">
+        <div className="center-bar-left" />
+        <div className="center-page">
+          <div className="center-top-bar" />
+          <CurvedEdge className="top-bar-edge-left" />
+          <CurvedEdge className="top-bar-edge-right" />
+          <div className="center-page-middle">
+            {props.children}
+          </div>
+          <CurvedEdge className="bottom-bar-edge-left" />
+          <CurvedEdge className="bottom-bar-edge-right" />
+          <div className="center-bottom-bar" />
+        </div>
+        <div className="center-bar-right" />
+      </div>
     </div>
   )
 }
