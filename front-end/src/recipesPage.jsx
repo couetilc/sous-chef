aa0e2a4b8263575ee389235f26dac7f06d687d14
Connor Couetil <couetilc@gmail.com>
2025-11-02 14:57:55 -0500

    styled recipe page to be legible on desktop and mobile

--- front-end/src/recipesPage.jsx
+++ front-end/src/recipesPage.jsx
@@ -30,57 +30,66 @@ export default function Recipes() {
   const clearFilters = () => {
     setEnteredName('')
     setFilterFavorites(false)
+    document.querySelector('input[name="recipeName"]').value = '';
   }
 
   return (
     <div className="recipes-page">
       <h1>Recipe Page</h1>
-      <div>
-        <form onSubmit={e => {
-          e.preventDefault()
-          const form = new FormData(e.target)
-          setEnteredName(form.get('recipeName'))
-        }}>
-          <input
-            name="recipeName"
-            placeholder='Search Recipe Name'
-          ></input>
-          <button type="submit">Search Name</button>
-        </form>
+      <div className="filter-bar">
+        <div className="filter-name">
+          <form onSubmit={e => {
+            e.preventDefault()
+            const form = new FormData(e.target)
+            setEnteredName(form.get('recipeName'))
+          }}>
+            <input
+              name="recipeName"
+              placeholder='Search Recipe Name'
+            ></input>
+            <button className="button" type="submit">Search Name</button>
+          </form>
+        </div>
+        <div className="filter-favorite">
+          <button className="button"
+            type="button"
+            onClick={onFilterFavorites}
+          >
+            Filter by Favorites ({filterFavorites.toString()})
+          </button>
+        </div>
+        <div className="filter-clear">
+          <button className="button" type="button" onClick={clearFilters}>Clear Filters</button>
+        </div>
       </div>
-      <div>
-        <button
-          type="button"
-          onClick={onFilterFavorites}
-        >
-          Filter by Favorites ({filterFavorites.toString()})
-        </button>
-      </div>
-      <div>
-        <button type="button" onClick={clearFilters}>Clear Filters</button>
-      </div>
-      <div>
+      <div className="paging">
         {recipes?.previous &&
-          <button onClick={() => setPage(p => p - 1)}>previous page</button>}
+          <button className="button" onClick={() => setPage(p => p - 1)}>previous page</button>}
         {recipes?.next &&
-          <button onClick={() => setPage(p => p + 1)}>next page</button>}
+          <button className="button" onClick={() => setPage(p => p + 1)}>next page</button>}
       </div>
-      <div>
+      <div className="recipes-list">
         {recipes?.results.map(recipe => (
-          <div key={recipe.id}>
-            <div>{recipe.title}</div>
-            <div>Ingredients:</div>
-            <ul>{recipe.ingredients.split('|').map(ingredient => (
-              <li>{ingredient.trim()}</li>
-            ))}</ul>
-            <div>Instructions:</div>
-            <ul>{recipe.instructions.split('|').map(step => (
-            <li>{step}</li>
-            ))}</ul>
-            {recipe.image_url &&
-              <img width="200px" src={recipe.image_url} loading="lazy"></img>}
+          <div key={recipe.id} className="recipe">
+            <h3>{recipe.title}</h3>
+            <div className="image-ingredients">
+              {recipe.image_url &&
+                <img width="200px" src={recipe.image_url} loading="lazy"></img>}
+              <div className="ingredients">
+                <h4>Ingredients:</h4>
+                <ul className="ingredients-list">{recipe.ingredients.split('|').map(ingredient => (
+                  <li>{ingredient.trim()}</li>
+                ))}</ul>
+              </div>
+            </div>
+            <div className="instructions">
+              <h4>Instructions:</h4>
+              <ul>{recipe.instructions.split('|').map(step => (
+              <li>{step}</li>
+              ))}</ul>
+            </div>
             {recipe.source_url &&
-              <a href={recipe.source_url} loading="lazy">source</a>}
+              <a className="source-url" href={recipe.source_url}>source</a>}
           </div>
         ))}
       </div>
