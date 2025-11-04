9121b543148f194664b3f74f1e52b96162f72898
Lance Salvador <lsalvad@purdue.edu>
2025-11-04 05:16:04 +0000

    Show count

--- front-end/src/recipesPage.jsx
+++ front-end/src/recipesPage.jsx
@@ -11,6 +11,7 @@ export default function Recipes() {
   const [filterFavorites, setFilterFavorites] = useState(false)
   const [page, setPage] = useState(1)
   const [recipes, setRecipes ] = useState();
+  const [ count, setCount ] = useState(0);
 
   useEffect(() => {
     const param = { page }
@@ -20,7 +21,10 @@ export default function Recipes() {
     if (filterFavorites) {
       param.searchFavorite = 'True'
     }
-    api.getRecipesFiltered(param).then(setRecipes)
+    api.getRecipesFiltered(param).then( (result) => {
+      setCount(result.count)
+      return result
+    }).then(setRecipes)
   }, [api, enteredName, filterFavorites, page])
 
   const onFilterFavorites = () => {
@@ -36,6 +40,7 @@ export default function Recipes() {
   return (
     <div className="recipes-page">
       <h1>Recipe Page</h1>
+      <h2>Displaying {count} recipes</h2>
       <div className="filter-bar">
         <div className="filter-name">
           <form onSubmit={e => {
