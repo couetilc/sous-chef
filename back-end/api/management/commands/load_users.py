0b689c95469fc5b259f1e1404097f668202e842f
Dalbert Sun <sun1170@purdue.edu>
2025-11-05 19:04:42 +0000

    load users script for testing and demoing is created

---
+++ back-end/api/management/commands/load_users.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create users for developers working on this project'
+
+    def handle(self, *args, **options):
+        users = [
+            # username, password, is_superuser
+            ('Dalbert', 'Password', False),
+            ('purdue', 'cs307team21', True)
+
+        ]
+        # Step 1: Delete existing demo user if exists
+        for username, password, is_superuser in users:
+            if User.objects.filter(username=username).exists():
+                self.stdout.write(
+                    f'Found existing user "{username}".'
+                )
+            else:
+                # create, optionally set superuser status
+                User.objects.create(username=username, password=password, is_superuser=is_superuser)
+                self.stdout.write(
+                    f'Created user "{username}".'
+                )
