4d367627e47a1f2993be90b9ab44b01046d2f90f
Connor Couetil <couetilc@gmail.com>
2025-11-06 15:26:57 -0500

    re-order items on the recipes page

--- front-end/src/recipesPage.jsx
+++ front-end/src/recipesPage.jsx
@@ -137,79 +137,79 @@ export default function Recipes() {
               <div className="image-ingredients">
                 {recipe.image_url &&
                   <img width="200px" src={recipe.image_url} loading="lazy" alt={recipe.title} />}
-                <div className="ingredients">
-                  <h4>Ingredients:</h4>
+
+                <div className="image-side-panel">
+                  <div className="time">
+                    <h4>Time:</h4>
+                    <ul>
+                      <li>Prep: {Number(recipe.prep_time_min) || 0} min</li>
+                      {hasCookTime(recipe.cook_time_min) && (
+                        <li>Cook: {Number(recipe.cook_time_min)} min</li>
+                      )}
+                      <li>Total: {Number(recipe.total_time_min) || 0} min</li>
+                    </ul>
+                  </div>
+
+                  {/* New: Price section */}
+                  {(pricePer || priceTotal) && (
+                    <div className="price" style={{ marginTop: '8px' }}>
+                      <h4>Price:</h4>
+                      <ul>
+                        {pricePer && <li>Per serving: {pricePer}</li>}
+                        {priceTotal && <li>Total: {priceTotal}</li>}
+                      </ul>
+                    </div>
+                  )}
+
+                  <div className="servings">
+                    <h4>Servings: {recipe.servings}</h4>
+                  </div>
+
+                  <div className="nutrition">
+                    <h4>Nutrition Calculator:</h4>
+                    <br />
+                    <label htmlFor={`servings-${recipe.id}`} style={{ marginRight: '6px' }}>
+                      # of servings:
+                    </label>
+                    <input
+                      id={`servings-${recipe.id}`}
+                      type="number"
+                      min="1"
+                      step="0.5"
+                      value={servingsInputs[recipe.id] ?? servingsMultipliers[recipe.id] ?? 1}
+                      onChange={(e) => handleServingsChange(recipe.id, e.target.value)}
+                      style={{ width: '60px' }}
+                    />
+                    <ul>
+                      <li>{"Calories: " + ((recipe.calories_per_serving || 0) * (servingsMultipliers[recipe.id] || 1)).toFixed(1)}</li>
+                      <li>{"Fat: " + ((recipe.fat_g || 0) * (servingsMultipliers[recipe.id] || 1)).toFixed(1) + "g"}</li>
+                      <li>{"Protein: " + ((recipe.protein_g || 0) * (servingsMultipliers[recipe.id] || 1)).toFixed(1) + "g"}</li>
+                      <li>{"Carbs: " + ((recipe.carbs_g || 0) * (servingsMultipliers[recipe.id] || 1)).toFixed(1) + "g"}</li>
+                    </ul>
+                  </div>
+
+                </div>
+              </div>
+
+              <div className="ingredients">
+                <details>
+                  <summary><h4>Ingredients:</h4></summary>
                   <ul className="ingredients-list">{recipe.ingredients.split('|').map((ingredient, i) => (
                     <li key={i}>{ingredient.trim()}</li>
                   ))}</ul>
-                </div>
+                </details>
               </div>
-
-              {/* New: Time section (shows prep, optional cook, total) */}
-              <div className="time">
-                <h4>Time:</h4>
-                <ul>
-                  <li>Prep: {Number(recipe.prep_time_min) || 0} min</li>
-                  {hasCookTime(recipe.cook_time_min) && (
-                    <li>Cook: {Number(recipe.cook_time_min)} min</li>
-                  )}
-                  <li>Total: {Number(recipe.total_time_min) || 0} min</li>
-                </ul>
-              </div>
-
-              {/* New: Price section */}
-              {(pricePer || priceTotal) && (
-                <div className="price" style={{ marginTop: '8px' }}>
-                  <h4>Price:</h4>
-                  <ul>
-                    {pricePer && <li>Per serving: {pricePer}</li>}
-                    {priceTotal && <li>Total: {priceTotal}</li>}
-                  </ul>
-                </div>
-              )}
 
               <div className="instructions" style={{ marginTop: '12px' }}>
-                <h4>Instructions:</h4>
-                <ul>{recipe.instructions.split('|').map((step, i) => (
-                  <li key={i}>{step}</li>
-                ))}</ul>
+                <details>
+                  <summary><h4>Instructions:</h4></summary>
+                  <ul>{recipe.instructions.split('|').map((step, i) => (
+                    <li key={i}>{step}</li>
+                  ))}</ul>
+                </details>
               </div>
 
-              <br />
 
-              <div className="servings">
-                <h4>Servings:</h4>
-                <ul>
-                  <li>{recipe.servings}</li>
-                </ul>
-              </div>
-
-              <br />
-
-              <div className="nutrition">
-                <h4>Nutrition:</h4>
-                <ul>
-                  <li>{"Calories: " + ((recipe.calories_per_serving || 0) * (servingsMultipliers[recipe.id] || 1)).toFixed(1)}</li>
-                  <li>{"Fat: " + ((recipe.fat_g || 0) * (servingsMultipliers[recipe.id] || 1)).toFixed(1) + "g"}</li>
-                  <li>{"Protein: " + ((recipe.protein_g || 0) * (servingsMultipliers[recipe.id] || 1)).toFixed(1) + "g"}</li>
-                  <li>{"Carbs: " + ((recipe.carbs_g || 0) * (servingsMultipliers[recipe.id] || 1)).toFixed(1) + "g"}</li>
-                </ul>
-              </div>
-
-              <div className="nutrition-multiplier" style={{ marginTop: '8px' }}>
-                <label htmlFor={`servings-${recipe.id}`} style={{ marginRight: '6px' }}>
-                  # of servings:
-                </label>
-                <input
-                  id={`servings-${recipe.id}`}
-                  type="number"
-                  min="1"
-                  step="0.5"
-                  value={servingsInputs[recipe.id] ?? servingsMultipliers[recipe.id] ?? 1}
-                  onChange={(e) => handleServingsChange(recipe.id, e.target.value)}
-                  style={{ width: '60px' }}
-                />
-              </div>
 
               <button
                 className={recipe.is_favorited ? "button-toggledOn" : "button"}
