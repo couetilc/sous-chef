60fff06b288d99faf372ae3645af74276eba1389
Lance Salvador <lsalvad@purdue.edu>
2025-11-07 02:31:02 +0000

    Add loading recipeingredients to prep script

--- bin/prep-db-for-demo.sh
+++ bin/prep-db-for-demo.sh
@@ -4,6 +4,7 @@ docker compose run --rm backend bash -c "\
 	python manage.py load_users && \
 	python manage.py load_ingredients && \
 	python manage.py load_recipes && \
+	python manage.py load_recipeingredients && \
 	python manage.py load_diets && \
 	python manage.py demo_recipe_history \
 	"
